e1ceaaab1e5618e4a6a73dfc5bbf0a942063a850
Sahib Singh <sdsahib95@gmail.com>
2018-02-05 19:28:19 +0530

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  property = 'navbar-primary';
+  left :boolean = true;
+  lArrow = '<i class="fas fa-chevron-circle-left"></i>';
+  rArrow = '<i class="fas fa-chevron-circle-right"></i>';
+  OnClick() : void{
+    if(this.property == 'navbar-primary'){
+      this.property = 'navbar-primary collapsed';
+      this.left =true;
+    }
+    else if(this.property == 'navbar-primary collapsed'){
+      this.property = 'navbar-primary';
+      this.left =false;
+    }
+    
+      console.log(this.property + this.left);
+  }
 }
